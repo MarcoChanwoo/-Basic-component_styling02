34f7fca9a094dcab52ad5a424e46b4437d5e9565
Marco <cwhong8087@gmail.com>
2023-12-04 10:13:54 +0900

    “updated”

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import SassComponent from "./SassComponent";
 
 // css Selector
 // -> css 클래스가 특정 클래스 내부에 있는 경우에만 스타일을 적용할 수 있다.
@@ -8,20 +9,24 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
-          <img src={logo} className="logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      // <div className="App">
+      //   <header>
+      //     <img src={logo} className="logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+
+      <div>
+        <SassComponent />
       </div>
     );
   }
